b80c08d0d789ca7f062a5ca9fdabf3f223ae599f
Paul McKillop <pgmckillop@gmail.com>
2020-09-16 20:57:24 +0100

    Button useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import { IonButton, IonToast } from "@ionic/react";
+import React, { useState } from "react";
 
 function App() {
+  const [showToast, setShowToast] = useState(false);
+  const handleClick = () => {
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 1500);
+  };
+
   return (
     <div>
       <header>
-        <h1>My App</h1>
-      </header>
+        <h1> My App </h1>{" "}
+      </header>{" "}
       <main>
-        <p>Add some content here…</p>
+        <IonButton onClick={handleClick}> Click Me </IonButton>{" "}
+        <IonToast isOpen={showToast} message="Hello world!" />
       </main>
     </div>
   );
